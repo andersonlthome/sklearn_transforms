ff8f29830d174ac4c94c231429fbca276634e49e
Anderson Lottermann Thomé <andersonlottermann@gmail.com>
2020-08-29 14:30:00 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,18 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+# Drop where NOTA=0 and the student cancel the class
+class DropLinesClassCancelStudent(BaseEstimator, TransformerMixin):     
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        return data.drop(data[
+                            (data["REPROVACOES_DE"] == 0) & (data["NOTA_DE"] == 0) | 
+                            (data["REPROVACOES_EM"] == 0) & (data["NOTA_EM"] == 0) |
+                            (data["REPROVACOES_MF"] == 0) & (data["NOTA_MF"] == 0) |
+                            (data["REPROVACOES_GO"] == 0) & (data["NOTA_GO"] == 0) |  
+                        ].index)   
+    
